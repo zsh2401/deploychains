c0353cf58718707196ce15946f48ffae247a2bd2
zsh2401 <zsh2401@163.com>
2023-07-23 23:26:59 +0800

    great

--- lib.sh
+++ lib.sh
@@ -1,15 +1,9 @@
 function dotenv(){
     export $(cat $TOOLS_PATH/.env | xargs)
-    # echo $TOOLS_PATH/.env 
-    # if [ ! -f $TOOLS_PATH/.env ]
-    # then
-    # cat $TOOLS_PATH/.env
-    # echo "fuck"
-    
-    # fi
 }
 function put(){
-    echo "Putting $1 to $2"
+    local last_argument=${!#}
+    echo "Transfering files to $last_argument"
     echo -e "put -r $1" | sftp -i $KEYFILE -oPort=$PORT $USERNAME@$HOST:$2
 }
 export -f put
--- var.sh
+++ var.sh
@@ -1,1 +1,2 @@
 export TOOLS_PATH=$(dirname "$(readlink -f "$0")")
+export CI=1
